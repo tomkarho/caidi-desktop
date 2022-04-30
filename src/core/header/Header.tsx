807cd4dcbffacde857b0c5b0fe42147ebc981789
tomkarho <tommi@tomkarho.com>
2022-04-30 12:21:06 +0300

    Add tab navigation

--- src/core/header/Header.tsx
+++ src/core/header/Header.tsx
@@ -5,7 +5,6 @@ import {NavLink} from 'react-router-dom';
 export function Header() {
     return (
         <header className="flex flex--center-h">
-            <h1>Caidi</h1>
             <nav>
                 <NavLink to="/">Extract</NavLink>
                 <NavLink to="/settings">Settings</NavLink>
