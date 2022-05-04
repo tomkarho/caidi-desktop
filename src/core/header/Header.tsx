5d624b490d38eff03cd2a34e6cad44bdca912f77
tomkarho <tommi@tomkarho.com>
2022-05-04 19:42:08 +0300

    Turn header navigation links into buttons

--- src/core/header/Header.tsx
+++ src/core/header/Header.tsx
@@ -1,13 +1,19 @@
 import './Header.scss';
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import {useAppSelector} from '../hooks';
 
 export function Header() {
+    const darkMode = useAppSelector(state => state.settings.darkMode);
     return (
-        <header className="flex flex--center-h">
+        <header className={`flex flex--center-h ${darkMode ? 'dark' : ''}`}>
             <nav>
-                <NavLink title="Navigate to extraction view" to="/">Extract</NavLink>
-                <NavLink title="Navigate to settings view" to="/settings">Settings</NavLink>
+                <NavLink title="Navigate to extraction view" to="/">
+                    <button>Extract</button>
+                </NavLink>
+                <NavLink title="Navigate to settings view" to="/settings">
+                    <button>Settings</button>
+                </NavLink>
             </nav>
         </header>
     );
