e24f7d65787de00620f98712b31bad03874ab3f9
tomkarho <tommi@tomkarho.com>
2022-05-06 19:13:35 +0300

    Get ffmpeg version from command line

--- public/electron/ffmpeg.js
+++ public/electron/ffmpeg.js
@@ -1,4 +1,22 @@
+const {exec} = require('child_process');
+const {logToFile} = require('./logging');
+
 function getFFMpegVersion(path) {
+    exec('ffmpeg -version',  (error, stdout, stderr) => {
+        if (error) {
+
+            logToFile(`getFFMpegVersion: An exception when attempting to get ffmpeg version ${error}`);
+            return null;
+        }
+
+        if (stderr) {
+            logToFile(`getFFMpegVersion: An error when attempting to get ffmpeg version ${error}`);
+            return null;
+        }
+
+        console.info('Stdout', stdout);
+    });
+
     return 'n5.0';
 }
 
