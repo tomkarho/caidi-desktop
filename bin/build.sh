b2d308d717d4ed1d32d800fccb307df33a84283b
tomkarho <tommi@tomkarho.com>
2022-05-18 23:26:08 +0300

    Use git data in archive names

--- bin/build.sh
+++ bin/build.sh
@@ -14,23 +14,23 @@ VERSION_HASH=$(git rev-parse --short HEAD)
 VERSION_NUMBER=$(git --no-pager log --oneline | wc -l)
 
 LINUX_FOLDER="caidi-linux-$VERSION_HASH-$VERSION_NUMBER"
-rm -rf $LINUX_FOLDER
+rm -rf $LINUX_FOLDER*
 mkdir $LINUX_FOLDER
 
 cp -r dist/linux-unpacked $LINUX_FOLDER/
 cp dist/*.AppImage $LINUX_FOLDER/
 cp dist/*.snap $LINUX_FOLDER/
 
-tar -zcf caidi-linux.tgz $LINUX_FOLDER/
+tar -zcf $LINUX_FOLDER.tgz $LINUX_FOLDER/
 rm -rf $LINUX_FOLDER
 
 echo "Packaging windows version"
 WINDOWS_FOLDER="caidi-windows-$VERSION_HASH-$VERSION_NUMBER"
-rm -rf $WINDOWS_FOLDER
+rm -rf $WINDOWS_FOLDER*
 mkdir $WINDOWS_FOLDER
 
 cp -r dist/win-unpacked $WINDOWS_FOLDER/
 cp dist/*.exe $WINDOWS_FOLDER/
 
-7z a caidi-windows.7z $WINDOWS_FOLDER/
+7z a $WINDOWS_FOLDER.7z $WINDOWS_FOLDER/
 rm -rf $WINDOWS_FOLDER
